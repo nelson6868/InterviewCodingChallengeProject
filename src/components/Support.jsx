6e1aa111d0d596cad4d9d57e6f3db347107769ea
nelson6868 <ekwomnelson090@gmail.com>
2021-11-02 22:43:55 +0100

    made some change on section

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -7,7 +7,7 @@ export default function Support() {
             <div className="row my-8">
            
                 <div className="col-8">
-                <h4 className="support">Supports</h4>
+                <h4 className="support">Support</h4>
                    
                     <h3 className="supportsubheader">Trusted by businessess all over Africa</h3>
                     <p className="supporttext">Our platform provides a resource of scalabe and reliable technology optimized
